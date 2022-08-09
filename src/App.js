cdb72c860338aa916bd76a5c04f4d66294ab5700
Matheus-Venancio <matheusvecordeiro@gmail.com>
2022-08-08 21:07:42 -0300

    initial calc

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
 import Resume from "./components/Resume"
 import GlobalStyle from "./styles/global";
 
 const App = () => {
+    const data = localStorage.getItem("transactions"); {/*COlocando o local storage em uma const */ }
+    const [transactionsList, setTransactionsList] = useState(//Vese se existe algo no localStorage
+        data ? JSON.parse(data) : [] //Se existir, ele vai fazer um parse para o JSON, caso nao, retona uma lista vazia
+    );
+
+    const [income, setIncome] = useState(0);//è as entradas no state
+    const [expense, setExpense] = useState(0);//è as saidas no state
+    const [total, setTotal] = useState(0);//è as total no state
+
+    useEffect (() => {
+      const amountExpense = transactionsList //Vai pegar da trnsdstionlist
+        .filter((item) => item.expense) //Filtra os items que sao saisas
+        .map((transaction) => Number(transaction.amount)) //Mapea os items que são os valores (pega os valores da saida)
+
+        const amountInconme = transactionsList //Vai pegar da trnsdstionlist
+        .filter((item) => !item.expense) //Filtra os items que sao saisas
+        .map((transaction) => Number(transaction.amount)) //Mapea os items que são os valores (pega os valores da saida)
+
+        const expense = amountExpense.reduce((acc, cur) => acc + cur,0).toFixed(2);
+        const income = amountInconme.reduce((acc, cur) => acc + cur,0).toFixed(2);
+
+        const total = Math.abs(income - expense).toFixed(2);
+
+        setIncome(`R$ ${income}`);
+        setExpense(`R$ ${expense}`);
+        setTotal(`${Number(income) < Number(expense) ? "-" : ""} R$ ${total}`);//se as entradas for menor que a saida, ele coloca uma saida de menos e depois coloca o total
+    }, [transactionsList]);//Qaundo mudar a lista de transação, ele muda o calculo
+
+
     return (
         <>
-        <Header />
-        <Resume />
-        <Form />
-        <GlobalStyle />
+            <Header />
+            <Resume income={income} expense={expense} total={total} />
+            <Form />
+            <GlobalStyle />
         </>
-    
+
     );
 };
 
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, {useState} from "react"; //import states
 import * as C from"./styles";
 
 
 const Form = () =>{
-    return <div>Teste</div>
+
+    const [desc, setDesc] = useState("");
+    const [amount, setAmount] = useState("");
+    const [isExpense, setExpense] = useState("");
+
+    const handleSave = () => {
+        if(!desc || !amount){
+            alert("Informe a descrição e o valor!");
+            return;
+        }else if(amount < 1){
+            alert("O valor tem que ser positivo");
+            return;
+        }
+    };
+
+
+    return( 
+    <C.Container>
+        <C.InputContent>
+            <C.Label>Descrição</C.Label>
+            <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} />
+        </C.InputContent>
+        <C.InputContent>
+            <C.Label>Value</C.Label>
+            <C.Input value={amount} type="number" onChange={(e) => setAmount(e.target.value)} />
+        </C.InputContent>
+        <C.RadioGroup>
+            <C.Input type="radio" id="rIncome" defaultChecked name="group1" onChange={() => setExpense(!isExpense)}  />
+            <C.Label htmlFor="rIncome">Entrada</C.Label>{/**Deixa selecionada a label**/}
+            <C.Input type="radio" id="rExpenses" defaultChecked name="group1" onChange={() => setExpense(!isExpense)}  />
+            <C.Label htmlFor="rExpenses">Saida</C.Label>{/**Deixa selecionada a label**/}
+        </C.RadioGroup>
+        <C.Button onClick={handleSave}>ADICIONAR</C.Button>
+    </C.Container>
+    )
 }
 
 export default Form
